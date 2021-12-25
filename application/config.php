20d7c6b24fdb2575f6acb50ce8e57c8c6b6c734f
Stephanie Ranegar <stephanie.ranegar@gmail.com>
2021-12-25 13:30:36 -0500

    Changing folder name
    Changed the folder name to gamePlanet instead of gameShack.

--- application/config.php
+++ application/config.php
@@ -13,8 +13,8 @@ error_reporting(E_ALL);
 date_default_timezone_set('America/New_York');
 
 //base url of the application
-define("BASE_URL", "http://localhost/i211-final/gameShack_mvc");
-//define("BASE_URL", "http://localhost/i211/gameShack_mvc");
+define("BASE_URL", "http://localhost/i211-final/gamePlanet-mvc");
+
 
 
 /*************************************************************************************
